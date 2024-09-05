b002ce3cb4c34ad1a2a64accdad54205dce8a2a7
lichiyuan <1025058301@qq.com>
2024-09-05 19:29:29 +0800

    project initial

---
+++ src/test/java/per/cy/personalwiki/PersonalWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package per.cy.personalwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
