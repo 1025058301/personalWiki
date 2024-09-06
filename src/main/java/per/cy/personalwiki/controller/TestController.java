539d60676ccf9564ab998e3719bfa50c424af582
lichiyuan <1025058301@qq.com>
2024-09-06 17:47:58 +0800

    新增配置文件，properties的优先级更高，尝试了自己增加手动配置

--- src/main/java/per/cy/personalwiki/controller/TestController.java
+++ src/main/java/per/cy/personalwiki/controller/TestController.java
@@ -1,12 +1,15 @@
 package per.cy.personalwiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//方法的返回值直接作为响应体，而不是视图名称
 public class TestController {
+    @Value("${my_config.name}")
+    private  String name;
     @RequestMapping("/hello")//支持所有http请求类型
-    public static String hello(){
-        return "hello world";
+    public  String hello(){
+        return "hello world"+" "+ name;
     }
 }
