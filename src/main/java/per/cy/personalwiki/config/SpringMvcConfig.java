c7eb853cc186457da97fbf9d3c8cb890c036a807
lichiyuan <1025058301@qq.com>
2024-09-14 20:41:23 +0800

    使用spring AOP，将切点定义在调用controller层之前，打印响应时间，请求参数和结果，屏蔽敏感字段，注释掉了之前使用的过滤器，拦截器

--- src/main/java/per/cy/personalwiki/config/SpringMvcConfig.java
+++ src/main/java/per/cy/personalwiki/config/SpringMvcConfig.java
@@ -1,22 +1,22 @@
-package per.cy.personalwiki.config;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import per.cy.personalwiki.Interceptor.LogInterceptor;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
-
+//package per.cy.personalwiki.config;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import per.cy.personalwiki.Interceptor.LogInterceptor;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
+//
