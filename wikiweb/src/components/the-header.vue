61d75e0f04e38a3a8f73323bbd3ad703774976c4
lichiyuan <1025058301@qq.com>
2024-09-15 21:45:11 +0800

    增加wiki管理页面和路由

--- wikiweb/src/components/the-header.vue
+++ wikiweb/src/components/the-header.vue
@@ -1,27 +1,33 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo"/>
     <a-menu
         theme="dark"
         mode="horizontal"
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 12311</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="home">
+        <router-link to="/">首页</router-link>
+      </a-menu-item>
+      <a-menu-item key="about">
+        <router-link to="/about">关于</router-link>
+      </a-menu-item>
+      <a-menu-item key="admin">
+        <router-link to="/admin/ebook">wiki书目管理</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { ref } from 'vue';
+import {defineComponent} from 'vue';
+import {ref} from 'vue';
 
 export default defineComponent({
   name: 'the-header',
   setup() {
     return {
-      selectedKeys1: ref<string[]>(['2'])
+      selectedKeys1: ref<string[]>(['home'])
     };
   },
 },);
