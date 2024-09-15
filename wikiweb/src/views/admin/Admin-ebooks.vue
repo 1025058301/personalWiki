827a6bf6781ccaf9795fae456a37cc537cde54b6
lichiyuan <1025058301@qq.com>
2024-09-15 22:22:54 +0800

    使用表格组件显示系统中所有的电子书，完成展示，分页等功能

--- wikiweb/src/views/admin/Admin-ebooks.vue
+++ wikiweb/src/views/admin/Admin-ebooks.vue
@@ -1,7 +1,131 @@
 <template>
-  <div class="about">
-    <h1>wiki管理页面</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks.books"
+          :pagination="pagination"
+          :loading="state.loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
 </template>
-<script setup lang="ts">
+
+<script lang="ts">
+import {defineComponent, onMounted, reactive, ref} from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const ebooks = reactive({books: []});
+    const state = reactive({loading: false});
+    const pagination = reactive({
+      current: 1,
+      pageSize: 3,
+      total: 0
+    });
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: {customRender: 'cover'}
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类一',
+        key: 'category1Id',
+        dataIndex: 'category1Id'
+      },
+      {
+        title: '分类二',
+        dataIndex: 'category2Id'
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      state.loading = true;
+      axios.get("/ebook/list", params).then((response) => {
+        state.loading = false;
+        const data = response.data;
+        ebooks.books = data.content;
+
+        // 重置分页按钮
+        pagination.current = params.page;
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    onMounted(() => {
+      handleQuery({});
+    });
+
+    return {
+      ebooks,
+      pagination,
+      columns,
+      state,
+      handleTableChange
+    }
+  }
+});
 </script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
