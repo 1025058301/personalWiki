98c993e3c13b58c4ae125972fb271070eda72815
lichiyuan <1025058301@qq.com>
2024-09-18 16:22:24 +0800

    修改首页，显示分类信息

--- wikiweb/src/views/Home.vue
+++ wikiweb/src/views/Home.vue
@@ -5,32 +5,19 @@
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span><user-outlined/>subnav 11111</span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1.items" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span><laptop-outlined/>subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span><notification-outlined/>subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -66,6 +53,8 @@
 import {UserOutlined, LaptopOutlined, NotificationOutlined} from '@ant-design/icons-vue';
 import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
 import axios from 'axios';
+import {buildTree, TreeNode} from "@/utils/tool";
+import {message} from "ant-design-vue";
 
 export default defineComponent({
   components: {
@@ -74,10 +63,33 @@ export default defineComponent({
     NotificationOutlined,
   },
   setup() {
+    const level1 = reactive<{ items: TreeNode[] }>({
+      items: []
+    }); // 一级分类树，children属性就是二级分类
+    let categorys: any[] = [];
+    /**
+     * 数据查询
+     **/
+    const handleCategory = () => {
+      state.loading = true;
+      axios.get("/category/all").then((response) => {
+        state.loading = false;
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content
+          console.log("原始数组：", categorys);
+          level1.items = buildTree(categorys, '0');
+          console.log("树形结构：", level1);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
     const ebooks = reactive({books: []});
     const state = reactive({loading: true});
     onMounted(() => {
-      console.log("on Mounted")
+      handleCategory();
+      console.log("on Mounted");
       axios.get("/ebook/list", {
         params: {page: 1, size: 100}
       }).then((response) => {
@@ -90,6 +102,8 @@ export default defineComponent({
     return {
       state,
       ebooks,
+      level1,
+      handleCategory,
       pagination: {
         onChange: (page: any) => {
           console.log(page);
