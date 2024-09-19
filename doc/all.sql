939466344b303c549192250b6a337ccb9fa8b1be
lichiyuan <1025058301@qq.com>
2024-09-19 16:13:33 +0800

    feature：创建用户表，生成pojo对象和持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -96,3 +96,14 @@ create table `demo` (
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `demo` (id, name) values (1, '测试');
+
+-- 用户表
+drop table if exists `user`;
+create table `user` (
+                        `id` bigint not null comment 'ID',
+                        `login_name` varchar(50) not null comment '登陆名',
+                        `name` varchar(50) comment '昵称',
+                        `password` char(32) not null comment '密码',
+                        primary key (`id`),
+                        unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
