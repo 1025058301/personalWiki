9ed307c3ba9a752ea3d76a85e0b645b284c54dbe
lichiyuan <1025058301@qq.com>
2024-09-21 20:07:54 +0800

    增加项目部署的配置文件

--- doc/all.sql
+++ doc/all.sql
@@ -5,8 +5,8 @@
 #                         `password` varchar(50) comment '密码',
 #                         primary key (`id`)
 # ) engine=innodb default charset=utf8mb4 comment='测试';
-
-
+#
+#
 # drop table if exists `demo`;
 # create table `demo` (
 #                         `id` bigint not null comment 'id',
@@ -14,7 +14,7 @@
 #                         primary key (`id`)
 # ) engine=innodb default charset=utf8mb4 comment='demo';
 # INSERT INTO `demo`(id, name) values (1,'测试mbg');
-# 电子书表
+#
 # drop table if exists `ebook`;
 # create table `ebook` (
 #                          `id` bigint not null comment 'id',
@@ -29,12 +29,12 @@
 #                          primary key (`id`)
 # ) engine=innodb default charset=utf8mb4 comment='电子书';
 #
-insert into `ebook` (id, name, description) values (1, 'Spring Boot 入门教程', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
-# 分类
+# insert into `ebook` (id, name, description) values (1, 'Spring Boot 入门教程', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
+# insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
+# insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
+# insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
+# insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+#
 # drop table if exists `category`;
 # create table `category` (
 #                             `id` bigint not null comment 'id',
@@ -59,68 +59,68 @@ insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '�
 # insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
 # insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
 # insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
-
-drop table if exists `doc`;
-create table `doc` (
-                       `id` bigint not null comment 'id',
-                       `ebook_id` bigint not null default 0 comment '电子书id',
-                       `parent` bigint not null default 0 comment '父id',
-                       `name` varchar(50) not null comment '名称',
-                       `sort` int comment '顺序',
-                       `view_count` int default 0 comment '阅读数',
-                       `vote_count` int default 0 comment '点赞数',
-                       primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='文档';
-
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (1, 1, 0, '文档1', 1, 0, 0);
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (2, 1, 1, '文档1.1', 1, 0, 0);
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (3, 1, 0, '文档2', 2, 0, 0);
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
-insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
-
-
-drop table if exists `content`;
-create table `content` (
-                           `id` bigint not null comment '文档id',
-                           `content` mediumtext not null comment '内容',
-                           primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='文档内容';
-
-
-drop table if exists `demo`;
-create table `demo` (
-                        `id` bigint not null comment 'id',
-                        `name` varchar(50) comment '名称',
-                        primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='测试';
-
-insert into `demo` (id, name) values (1, '测试');
-
--- 用户表
-drop table if exists `user`;
-create table `user` (
-                        `id` bigint not null comment 'ID',
-                        `login_name` varchar(50) not null comment '登陆名',
-                        `name` varchar(50) comment '昵称',
-                        `password` char(32) not null comment '密码',
-                        primary key (`id`),
-                        unique key `login_name_unique` (`login_name`)
-) engine=innodb default charset=utf8mb4 comment='用户';
-
-insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '测试', 'test');
-
-
--- 电子书快照表
-drop table if exists `ebook_snapshot`;
-create table `ebook_snapshot` (
-                                  `id` bigint auto_increment not null comment 'id',
-                                  `ebook_id` bigint not null default 0 comment '电子书id',
-                                  `date` date not null comment '快照日期',
-                                  `view_count` int not null default 0 comment '阅读数',
-                                  `vote_count` int not null default 0 comment '点赞数',
-                                  `view_increase` int not null default 0 comment '阅读增长',
-                                  `vote_increase` int not null default 0 comment '点赞增长',
-                                  primary key (`id`),
-                                  unique key `ebook_id_date_unique` (`ebook_id`,`date`)
-) engine=innodb default charset=utf8mb4 comment='电子书快照表';
+#
+# drop table if exists `doc`;
+# create table `doc` (
+#                        `id` bigint not null comment 'id',
+#                        `ebook_id` bigint not null default 0 comment '电子书id',
+#                        `parent` bigint not null default 0 comment '父id',
+#                        `name` varchar(50) not null comment '名称',
+#                        `sort` int comment '顺序',
+#                        `view_count` int default 0 comment '阅读数',
+#                        `vote_count` int default 0 comment '点赞数',
+#                        primary key (`id`)
+# ) engine=innodb default charset=utf8mb4 comment='文档';
+#
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (1, 1, 0, '文档1', 1, 0, 0);
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (2, 1, 1, '文档1.1', 1, 0, 0);
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (3, 1, 0, '文档2', 2, 0, 0);
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (4, 1, 3, '文档2.1', 1, 0, 0);
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, '文档2.2', 2, 0, 0);
+# insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+#
+#
+# drop table if exists `content`;
+# create table `content` (
+#                            `id` bigint not null comment '文档id',
+#                            `content` mediumtext not null comment '内容',
+#                            primary key (`id`)
+# ) engine=innodb default charset=utf8mb4 comment='文档内容';
+#
+#
+# drop table if exists `demo`;
+# create table `demo` (
+#                         `id` bigint not null comment 'id',
+#                         `name` varchar(50) comment '名称',
+#                         primary key (`id`)
+# ) engine=innodb default charset=utf8mb4 comment='测试';
+#
+# insert into `demo` (id, name) values (1, '测试');
+#
+# -- 用户表
+# drop table if exists `user`;
+# create table `user` (
+#                         `id` bigint not null comment 'ID',
+#                         `login_name` varchar(50) not null comment '登陆名',
+#                         `name` varchar(50) comment '昵称',
+#                         `password` char(32) not null comment '密码',
+#                         primary key (`id`),
+#                         unique key `login_name_unique` (`login_name`)
+# ) engine=innodb default charset=utf8mb4 comment='用户';
+#
+# insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '测试', 'test');
+#
+#
+# -- 电子书快照表
+# drop table if exists `ebook_snapshot`;
+# create table `ebook_snapshot` (
+#                                   `id` bigint auto_increment not null comment 'id',
+#                                   `ebook_id` bigint not null default 0 comment '电子书id',
+#                                   `date` date not null comment '快照日期',
+#                                   `view_count` int not null default 0 comment '阅读数',
+#                                   `vote_count` int not null default 0 comment '点赞数',
+#                                   `view_increase` int not null default 0 comment '阅读增长',
+#                                   `vote_increase` int not null default 0 comment '点赞增长',
+#                                   primary key (`id`),
+#                                   unique key `ebook_id_date_unique` (`ebook_id`,`date`)
+# ) engine=innodb default charset=utf8mb4 comment='电子书快照表';
--- src/test/java/per/cy/personalwiki/PersonalWikiApplicationTests.java
+++ src/test/java/per/cy/personalwiki/PersonalWikiApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package per.cy.personalwiki;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class PersonalWikiApplicationTests {
     }
 
 }
+*/
