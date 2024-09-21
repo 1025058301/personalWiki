3c52ab94aa8c6d5fc86ab207b2cb540da5654098
lichiyuan <1025058301@qq.com>
2024-09-21 13:14:48 +0800

    feature: 增加e-chart的依赖

--- wikiweb/src/components/the-welcome.vue
+++ wikiweb/src/components/the-welcome.vue
@@ -87,13 +87,19 @@
         </a-card>
       </a-col>
     </a-row>
+    <br>
+    <a-row>
+      <a-col :span="24">
+        <div id="main" style="width: 100%;height:300px;"></div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent, ref, onMounted, reactive} from 'vue'
 import axios from 'axios';
-
+declare let echarts: any;
 export default defineComponent({
   name: 'the-welcome',
   setup() {
@@ -121,15 +127,44 @@ export default defineComponent({
           const nowRate = (now.getHours() * 60 + now.getMinutes()) / (60 * 24);
           // console.log(nowRate)
           statistic.todayViewIncrease = parseInt(String(statisticResp[1].viewIncrease / nowRate));
+          const viewIncrease0 = statisticResp[0].viewIncrease == 0? 1 : statisticResp[0].viewIncrease;
           // todayViewIncreaseRate：今日预计增长率
-          statistic.todayViewIncreaseRate = (statistic.todayViewIncrease - statisticResp[0].viewIncrease) / statisticResp[0].viewIncrease * 100;
+          statistic.todayViewIncreaseRate = (statistic.todayViewIncrease - viewIncrease0) / viewIncrease0 * 100;
           statistic.todayViewIncreaseRateAbs = Math.abs(statistic.todayViewIncreaseRate);
         }
       });
     };
+    const testEcharts = () => {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById('main'));
+
+      // 指定图表的配置项和数据
+      const option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data:['销量']
+        },
+        xAxis: {
+          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    };
 
     onMounted(() => {
       getStatistic();
+      testEcharts();
     });
 
     return {
